f4f9be3f56ba83c50c7cad2057ea592184985867
Isaac Luo <yishaluo@gmail.com>
2020-01-30 17:08:22 +0000

    move userMust functions

---
+++ api/src/userMust.ts
@@ -0,0 +1,34 @@
+import koa from 'koa';
+type Ctx = koa.ParameterizedContext<ICustomState, {}>;
+type Next = ()=>Promise<any>;
+
+export function userMust (...args: Array<(ctx:koa.ParameterizedContext<any, {}>, next:()=>Promise<any>)=>boolean>) {
+  const arg = arguments;
+  return async (ctx:koa.ParameterizedContext<any, {}>, next:Next)=> {
+    if (Array.prototype.some.call(arg, f=>f(ctx))) {
+      await next();
+    } else {
+      ctx.throw(401);
+    }
+  };
+}
+
+export function beUser (ctx:Ctx, next?:Next) {
+  // console.log(ctx.state.user.groups);
+  return !!(ctx.state.user && (ctx.state.user.groups.indexOf('emma/users')>=0 || ctx.state.user.groups.indexOf('users')>=0));
+  // return ctx.state.user!== undefined;
+}
+
+export function beAnyOne (ctx:Ctx, next?:Next) {
+  // console.log(ctx.state.user.groups);
+  // return ctx.state.user && (ctx.state.user.groups.indexOf('emma/users')>=0 || ctx.state.user.groups.indexOf('users')>=0);
+  return ctx.state.user!== undefined;
+}
+
+export function beAdmin (ctx:Ctx, next?:Next) {
+  return ctx.state.user && (ctx.state.user.groups.indexOf('administrators')>=0 || ctx.state.user.groups.indexOf('emma/administrators')>=0);
+}
+
+export function beGuest (ctx:Ctx, next?:Next) {
+  return ctx.state.user === undefined || ctx.state.user._id === '000000000000000000000000';
+}
