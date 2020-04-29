e8f11875e8040a2931b54e12f3be901eca230f16
Isaac Luo <yishaluo@gmail.com>
2020-04-29 22:07:38 +0100

    fixed bugs when importDB with authSource option

--- api/utility/import_data.py
+++ api/utility/import_data.py
@@ -27,8 +27,9 @@ black_list_feature = ['contig', 'region']
 conf_mongo = conf['secret']['mongoDB']
 # mongo_uri, mongo_db, mongo_auth = re.findall(r'^(.+)/(.+)\?authSource=(.+)',conf_mongo['url'])[0]
 # client = pymongo.MongoClient(conf_mongo['url'], username=conf_mongo['username'], password=conf_mongo['password'])
-mongo_uri, mongo_db = re.findall(r'^(.+)/(.+)',conf_mongo['url'])[0]
+mongo_uri, mongo_db_rest_part = re.findall(r'^(.+)/(.+)',conf_mongo['url'])[0]
 client = pymongo.MongoClient(conf_mongo['url'])
+mongo_db, _ = mongo_db_rest_part.split('?')
 db = client[mongo_db]
 ProjectFolder = db['project_folders']
 Project = db['projects']
